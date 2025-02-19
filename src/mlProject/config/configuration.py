b7d3ad434899459d790b49dabae4c37f7010062d
Akshansh <akshanshz@gmail.com>
2025-02-19 16:28:44 +0100

    configuration.py ensure annotation error fix

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -16,8 +16,8 @@ class ConfigurationManager:
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
-
-        create_directories(config.root_dir)
+        print(config.root_dir)
+        create_directories([config.root_dir])
 
         data_ingestion_config = DataIngestionConfig(
             root_dir=config.root_dir,
