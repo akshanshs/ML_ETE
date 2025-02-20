3ffaec8f53060f32229fb4745266d71837992cc2
Akshansh <akshanshz@gmail.com>
2025-02-20 11:43:23 +0100

    data transformation entity, componenet, pipeline added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.mlProject.pipeline.stage_03_data_transformation import DataTransformationPipeline
 
 STAGE_NAME="Data Ingestion stage"
 
@@ -18,6 +19,17 @@ STAGE_NAME = "Data Validation stage"
 try:
     logger.info(f">>>> {STAGE_NAME} started <<<<")
     obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> {STAGE_NAME} completed <<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f">>>> {STAGE_NAME} started <<<<")
+    obj = DataTransformationPipeline()
     obj.main()
     logger.info(f">>>> {STAGE_NAME} completed <<<<\n\nx==========x")
 except Exception as e:
